05df737a1fde75f277c1404a2d422704874fb7e0
snys98 <snys98@outlook.com>
2017-05-04 19:00:51 +0800

    add state machine implemention of custom IAsyncStateMachine

--- AsyncStateMechineTest/Program.cs
+++ AsyncStateMechineTest/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
@@ -101,17 +104,42 @@ namespace AsyncStateMechineTest
                         }
                     }
                 }
-            }; 
+            };
             #endregion
-            var task = Travle(node);
-            while (true)
+
+            Console.WriteLine($@"
+1.state machine implemention of async&await
+2.state machine implemention of custom IAsyncStateMachine");
+            var selection = Console.ReadKey().Key;
+            Console.WriteLine();
+            Task<int> task;
+            switch (selection)
             {
-                if (task.IsCompleted)
-                {
-                    Console.WriteLine("线程id:"+ Thread.CurrentThread.ManagedThreadId +"|线程上下文标识:"+ Thread.CurrentThread.ExecutionContext.GetHashCode() + ":" + task.Result);
+                case ConsoleKey.D1:
+                    task = Travle(node);
+                    while (true)
+                    {
+                        if (task.IsCompleted)
+                        {
+                            Console.WriteLine("线程id:" + Thread.CurrentThread.ManagedThreadId + "|线程上下文标识:" + Thread.CurrentThread.ExecutionContext.GetHashCode() + ":" + task.Result);
+                            break;
+                        }
+                    }
                     break;
-                }
+                case ConsoleKey.D2:
+                    task = StateMechineTravle(node);
+                    while (true)
+                    {
+                        if (task.IsCompleted)
+                        {
+                            Console.WriteLine("线程id:" + Thread.CurrentThread.ManagedThreadId + "|线程上下文标识:" + Thread.CurrentThread.ExecutionContext.GetHashCode() + ":" + task.Result);
+                            break;
+                        }
+                    }
+                    break;
             }
+
+
             Console.ReadKey();
         }
 
@@ -125,31 +153,129 @@ namespace AsyncStateMechineTest
                 Console.WriteLine("线程id:" + Thread.CurrentThread.ManagedThreadId + "|线程上下文标识:" + Thread.CurrentThread.ExecutionContext.GetHashCode() + ":" + await Travle(node.Right));
             }
             return node.Id;
-            //   StateMechine stateMechine = new StateMechine();
-            //stateMechine.node = node;
-            //stateMechine.builder = AsyncTaskMethodBuilder<int>.Create();
-            //stateMechine.state = -1;
-            //AsyncTaskMethodBuilder<int> next_builder = stateMechine.builder;
-            //   next_builder.Start<StateMechine>(ref stateMechine);
-            //return stateMechine.builder.Task;
+
         }
 
+        public static Task<int> StateMechineTravle(Node node)
+        {
+            StateMechine stateMechine = new StateMechine();
+            stateMechine.node = node;
+            stateMechine.builder = AsyncTaskMethodBuilder<int>.Create();
+            stateMechine.state = -1;
+            AsyncTaskMethodBuilder<int> next_builder = stateMechine.builder;
+            next_builder.Start<StateMechine>(ref stateMechine);
+            return stateMechine.builder.Task;
+        }
     }
 
-    public class StateMechine:IAsyncStateMachine
+    public struct StateMechine : IAsyncStateMachine
     {
-        public Node node { get; set; }
+        public int state;
 
-        public AsyncTaskMethodBuilder<int> builder { get; set; }
-        public int state { get; set; }
-        public void MoveNext()
+        public AsyncTaskMethodBuilder<int> builder;
+
+        public Node node;
+
+        private object wrap1;
+
+        private object wrap2;
+
+        private TaskAwaiter<int> awaiter;
+
+        void IAsyncStateMachine.MoveNext()
         {
-            return;
+            int num = this.state;
+            int id;
+            try
+            {
+                TaskAwaiter<int> taskAwaiter;
+                if (num != 0)
+                {
+                    if (num == 1)
+                    {
+                        taskAwaiter = this.awaiter;
+                        this.awaiter = default(TaskAwaiter<int>);
+                        this.state = -1;
+                        goto IL_1AB;
+                    }
+                    this.node.IsTraveled = true;
+                    if (this.node.IsLeaf)
+                    {
+                        goto IL_20C;
+                    }
+                    this.wrap1 = Thread.CurrentThread.ManagedThreadId;
+                    this.wrap2 = Thread.CurrentThread.ExecutionContext.GetHashCode();
+                    taskAwaiter = Program.Travle(this.node.Left).GetAwaiter();
+                    if (!taskAwaiter.IsCompleted)
+                    {
+                        this.state = 0;
+                        this.awaiter = taskAwaiter;
+                        this.builder.AwaitUnsafeOnCompleted<TaskAwaiter<int>, StateMechine>(ref taskAwaiter, ref this);
+                        return;
+                    }
+                }
+                else
+                {
+                    taskAwaiter = this.awaiter;
+                    this.awaiter = default(TaskAwaiter<int>);
+                    this.state = -1;
+                }
+                int arg_CC_0 = taskAwaiter.GetResult();
+                taskAwaiter = default(TaskAwaiter<int>);
+                object obj = arg_CC_0;
+                Console.WriteLine(string.Concat(new object[]
+                {
+                    "线程id:",
+                    this.wrap1,
+                    "|线程上下文标识:",
+                    this.wrap2,
+                    ":",
+                    obj
+                }));
+                this.wrap1 = null;
+                this.wrap2 = null;
+                this.wrap2 = Thread.CurrentThread.ManagedThreadId;
+                this.wrap1 = Thread.CurrentThread.ExecutionContext.GetHashCode();
+                taskAwaiter = Program.Travle(this.node.Right).GetAwaiter();
+                if (!taskAwaiter.IsCompleted)
+                {
+                    this.state = 1;
+                    this.awaiter = taskAwaiter;
+                    this.builder.AwaitUnsafeOnCompleted<TaskAwaiter<int>, StateMechine>(ref taskAwaiter, ref this);
+                    return;
+                }
+                IL_1AB:
+                int arg_1BA_0 = taskAwaiter.GetResult();
+                taskAwaiter = default(TaskAwaiter<int>);
+                obj = arg_1BA_0;
+                Console.WriteLine(string.Concat(new object[]
+                {
+                    "线程id:",
+                    this.wrap2,
+                    "|线程上下文标识:",
+                    this.wrap1,
+                    ":",
+                    obj
+                }));
+                this.wrap2 = null;
+                this.wrap1 = null;
+                IL_20C:
+                id = this.node.Id;
+            }
+            catch (Exception exception)
+            {
+                this.state = -2;
+                this.builder.SetException(exception);
+                return;
+            }
+            this.state = -2;
+            this.builder.SetResult(id);
         }
 
-        public void SetStateMachine(IAsyncStateMachine stateMachine)
+        [DebuggerHidden]
+        void IAsyncStateMachine.SetStateMachine(IAsyncStateMachine stateMachine)
         {
-            return;
+            this.builder.SetStateMachine(stateMachine);
         }
     }
 }
